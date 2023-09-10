e477f2cdc5960807b10e071d909ae968e5b7c843
Roberto Jobet <jobetinfosec@gmail.com>
2023-09-10 15:32:08 +0200

    Updating

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ locals {
   environment  = var.environment
 }
 
+# create vpc
 module "vpc" {
   source                       = "git@github.com:jobetinfosec/3tier-terraform-modules.git//vpc"
   region                       = local.region
@@ -16,4 +17,19 @@ module "vpc" {
   private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# create nat gateways
+module "nat_gateway" {
+  source                     = "git@github.com:jobetinfosec/3tier-terraform-modules.git//nat-gateway"
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  internet_gateway           = module.vpc.internet_gateway
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
 }
